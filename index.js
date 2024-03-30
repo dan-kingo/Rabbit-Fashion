ca702c4a1f26db143bac6520fe3dc4db1dfd84b7
Dan Kingo <danieldejen23@gmail.com>
2024-03-30 15:04:44 +0300

    add site preloader

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ let sidebar = document.querySelector(".sidebar");
 document.getElementById("btn").onclick = function () {
   sidebar.classList.toggle("active");
 };
+
+let loader = document.querySelector(".loader-container");
+let page = document.querySelector(".main-content");
+
+window.addEventListener("load", () => {
+  loader.classList.add("hidden");
+  page.classList.add("visible");
+});
